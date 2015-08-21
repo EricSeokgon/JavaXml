36ba39af2078837f23f3c7c39a9c4321dba029d6
hadesleee <leesk55@gmail.com>
2015-08-21 09:36:42 +0900

    Initial commit

---
+++ src/JDOMXMLParser/SAXParserDemo.java
@@ -0,0 +1,90 @@
+package JDOMXMLParser;
+
+/**
+ * Project: TutorialsPointJava8
+ * FileName: SAXParserDemo
+ * Date: 2015-08-21
+ * Time: 오전 9:11
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+import java.io.File;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXParserDemo {
+    public static void main(String[] args){
+
+        try {
+            File inputFile = new File("C:\\Users\\sklee\\IdeaProjects\\JavaXml\\src\\JDOMXMLParser\\input.txt");
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            UserHandler userhandler = new UserHandler();
+            saxParser.parse(inputFile, userhandler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class UserHandler1 extends DefaultHandler {
+
+    boolean bFirstName = false;
+    boolean bLastName = false;
+    boolean bNickName = false;
+    boolean bMarks = false;
+
+    @Override
+    public void startElement(String uri,
+                             String localName, String qName, Attributes attributes)
+            throws SAXException {
+        if (qName.equalsIgnoreCase("student")) {
+            String rollNo = attributes.getValue("rollno");
+            System.out.println("Roll No : " + rollNo);
+        } else if (qName.equalsIgnoreCase("firstname")) {
+            bFirstName = true;
+        } else if (qName.equalsIgnoreCase("lastname")) {
+            bLastName = true;
+        } else if (qName.equalsIgnoreCase("nickname")) {
+            bNickName = true;
+        }
+        else if (qName.equalsIgnoreCase("marks")) {
+            bMarks = true;
+        }
+    }
+
+    @Override
+    public void endElement(String uri,
+                           String localName, String qName) throws SAXException {
+        if (qName.equalsIgnoreCase("student")) {
+            System.out.println("End Element :" + qName);
+        }
+    }
+
+    @Override
+    public void characters(char ch[],
+                           int start, int length) throws SAXException {
+        if (bFirstName) {
+            System.out.println("First Name: "
+                    + new String(ch, start, length));
+            bFirstName = false;
+        } else if (bLastName) {
+            System.out.println("Last Name: "
+                    + new String(ch, start, length));
+            bLastName = false;
+        } else if (bNickName) {
+            System.out.println("Nick Name: "
+                    + new String(ch, start, length));
+            bNickName = false;
+        } else if (bMarks) {
+            System.out.println("Marks: "
+                    + new String(ch, start, length));
+            bMarks = false;
+        }
+    }
+}
